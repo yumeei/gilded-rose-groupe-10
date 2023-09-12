c65eca5725f23614e3e751695c4d949d85035180
Valérie Song <valerie.song@ormex.io>
2023-09-13 01:00:13 +0200

    refacto index.ts + jest test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    preset: 'ts-jest',
+    transform: {
+        '^.+\\.(ts|tsx)?$': 'ts-jest',
+        '^.+\\.(js|jsx)$': 'babel-jest',
+    }
 };
